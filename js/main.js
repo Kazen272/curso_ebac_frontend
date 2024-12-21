de797fff217ada6d9d167586b5b2f3b72d8e672e
Kazen272 <cadudps@gmail.com>
2024-12-21 01:10:27 -0300

    Validações feitas

--- js/main.js
+++ js/main.js
@@ -3,16 +3,25 @@ const form = document.getElementById("formulario");
 form.addEventListener('submit', function (e) {
     let formValido = false;
     e.preventDefault();
-}
-);
+});
 
 function validaNumero() {
     const numeroA = parseFloat(document.getElementById("numberA").value);
     const numeroB = parseFloat(document.getElementById("numberB").value);
+    const mensagemSucesso = document.querySelector(".success-message");
+    const mensagemErro = document.querySelector(".error-message");
 
     if (numeroB > numeroA){
-        return alert("Numero B é maior que o Numero A");
+        mensagemSucesso.style.display = "block"; // Mostra a mensagem de sucesso
+        mensagemErro.style.display = "none";    // Esconde a mensagem de erro
     }else{
-        alert("Numero B não é maior que o Numero A");
+        mensagemSucesso.style.display = "none"; // Esconde a mensagem de sucesso
+        mensagemErro.style.display = "block";  // Mostra a mensagem de erro
+    if (!numeroA && !numeroB) {
+        mensagemSucesso.style.display = "none"; // Esconde a mensagem de sucesso
+        mensagemErro.style.display = "none"; // Esconde a mensagem de sucesso
+        }
     }
-}
+    
+} 
+
