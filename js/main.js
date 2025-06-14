f2256184371a52c44b05d220d6fcb5ed53f57b01
Kazen272 <cadudps@gmail.com>
2025-06-14 01:31:31 -0300

    dinamismo na inserção das ULs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
-$('form').on('submit', function (e) {
-    e.preventDefault();
+$(document).ready(function() {
+    // Evento de submit do formulário
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        // Capturar o valor da tarefa
+        var tarefa = $('#tarefa').val();
+
+        // Limpar o campo de input
+        $('#tarefa').val('');
+
+        // Adicionar a tarefa na lista
+        $('#listaTarefas').append('<li><a href="#">' + tarefa + '</a></li>');
+    });
+
+    // Evento de clique nas li (usando delegação de eventos)
+    $('#listaTarefas').on('click', 'li', function(e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
+        $(this).toggleClass('tarefa-Concluida');
+    });
+});
